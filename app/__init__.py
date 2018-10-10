5fb47053ea424b5827b69323e10f14c60ab15f1b
ObFahad <alobaid.a.f@gmail.com>
2018-10-11 00:43:15 +0300

    Add phaser configuration to run on flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = './templates')
 app.config['SECRET_KEY'] = 'secret!'
+
 socketio = SocketIO(app, async_mode = None)
+
 
 from app import routes
